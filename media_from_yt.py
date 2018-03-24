fd836e9fab043484595f55c225cd272765b5e119
Danyal Ahsanullah <danyal.ahsanullah@gmail.com>
2018-03-24 18:52:37 -0500

    revamped backend, added basic metadata support and slicing of chapter'd yt vids
    * initial branch commit
    
    * moved raw json responses to ref
    
    * moved raw json responses to ref
    
    * modified gitignore, streamlined functions
    
    * added to gitignore
    
    * expiremental soundcloud displaying of metadata
    
    * cleanup
    
    * support slicing on youtube indexed chapter fields
    
    * implemented new_backend with usual interface

--- media_from_yt.py
+++ media_from_yt.py
@@ -1,49 +1,51 @@
 #! /usr/bin/env python3
 # vim:fileencoding=utf-8
 # -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+import re
+import os
+import sys
+import logging
+import argparse
+import youtube_dl
+import os.path as osp
+
 """
 media_from_yt.py
 converts youtube audio streams to audio files. 
-uses pafy internally, thus ffmpeg or libav is required and youtube-dl is strongly reccomended.
+uses youtube-dl internally, thus ffmpeg or libav is required and youtube-dl is strongly recommended.
 
 
 requires:
-pip install pafy
-pip install pydub
 ffmpeg or libav
+youtube-dl
 
-reccomended:
-pip install youtube-dl
+optional: 
+pydub  -- audio slicing
 
-optional:
-pip install soundscrape
+mutagen --  metadata tagging (may be removed with youtube-dl / pydub capabilities)
 
 example usage: 
 media_from_yt <youtube id> 
 """
 
-import os
-import sys
-import pafy
-import argparse
-import logging
-from glob import glob
-from pydub import AudioSegment
-from soundscrape import soundscrape as sc
-
+try: 
+    from pydub import AudioSegment
+    # from mutagen.mp3 import EasyMP3 as MP3
+    PYDUB = True
+except ImportError:
+    # dont like this... fix it
+    import warnings
+    warnings.warn('Unable to Find pydub in PYTHONPATH, slicing of audio segments is not supported without it.')
+    del warnings
+    PYDUB = False
+    
 __author__ = 'Danyal Ahsanullah'
-__version_info__ = (0,6)
+__version_info__ = (1,0)
 __version__ = '.'.join(map(str, __version_info__))
 
 COMMENT = '##'
-
-#todo: add in soundscrape capability -- probably rename to media_from_stream
-#todo: look into pytube for maybe removing some dependencies.
-
-class BitrateException(Exception):
-    """Best detected bitrate is not within bitrate tolerance"""
-    def __init__(self, *args, **kwargs):
-        super(BitrateException, self).__init__("Best detected bitrate is not within bitrate tolerance")
+OUTPUTDIR = 'output'
 
 
 # logger creation
@@ -55,134 +57,243 @@ ch.setLevel(logging.WARNING)
 ch.setFormatter(formatter)
 logger.addHandler(ch)
 
+# cli parser definition
 parser = argparse.ArgumentParser('media_from_yt')
-parser.add_argument('urls', type=str, nargs='*', help='url(s) or id(s) of youtube vids to be converted.')
-parser.add_argument('--output', '-o', type=str, help='base directory to save results into.')
-parser.add_argument('--extension', '--ext', '-e', type=str, default='mp3',
-                    help='extension to use for file(s) to be created.'
-                    )
-parser.add_argument('--bitrate', '-b', type=str, default='128k', help='desired bitrate to have exported file(s) in.')
-
+parser.add_argument('urls', type=str, nargs='+', help='url(s) or id(s) of media to be converted')
+parser.add_argument('--output', '-o', type=str, metavar='PATH', help='base directory to save results into')
+parser.add_argument('--extension', '-e', type=str, metavar='EXT',default='mp3',
+                                    help='extension to use for file(s) to be created')
 parser.add_argument('--file', '-f', action='store_true',
-                    help='flag that if set, treats urls as a file with a list of urls inside it (one per line)'
-                    )
+                                    help='flag that if set, treats urls as a file with a list of urls inside it'
+                                             '(one per line, comments start with "{}")'.format(COMMENT))
+parser.add_argument('--no-metadata', action='store_true', help='if passed, prevents metadata from being added')
 
-loudness = parser.add_mutually_exclusive_group()
-loudness.add_argument('--verbose', '-v', action='count', help='prints a more detailed output', default=0)
-loudness.add_argument('--quiet', '-q', action='count', help='suppresses console output', default=0)
+parser_quality = parser.add_mutually_exclusive_group()        
+parser_quality.add_argument('--bitrate', '-br', type=str, metavar='BR',default='', 
+                                                help='desired bitrate to have exported file(s) in')
+parser_quality.add_argument('--quality', '-qa', type=str, metavar='QA',default='2', 
+                                                help='desired vbr quality to have exported file(s) in')
 
-source = parser.add_mutually_exclusive_group()
-source.add_argument('--soundcloud', '-S', action='store_true', help='treats inputs as soundcloud links')
-source.add_argument('--bandcamp', '-B', action='store_true', help='treats inputs as bandcamp links')
+parser_loudness = parser.add_mutually_exclusive_group()
+parser_loudness.add_argument('--verbose', '-v', action='count', help='prints a more detailed output', default=0)
+parser_loudness.add_argument('--quiet', '-q', action='count', help='suppresses console output', default=0)
+
+# source = parser.add_mutually_exclusive_group()
+# source.add_argument('--soundcloud', '-S', action='store_true', help='treats inputs as soundcloud links')
+# source.add_argument('--bandcamp', '-B', action='store_true', help='treats inputs as bandcamp links')
 
 parser.add_argument('--version', '-V', action='version', version="%(prog)s " + __version__)
 
 
+
+def my_hook(d):
+    """
+    d will have keys:
+    
+    [downloading]
+    'status'
+    'downloaded_bytes'
+    'total_bytes'
+    'tmpfilename'
+    'filename'
+    'eta'
+    'speed'
+    'elapsed'
+    '_eta_str'
+    '_percent_str'
+    '_speed_str'
+    '_total_bytes_str'
+    
+    [finished]
+    'filename'
+    'status'
+    'total_bytes'
+    '_total_bytes_str'
+    """
+    if d['status'] == 'finished':
+        print('\nDone downloading, now converting ...')
+    else:
+        # print(d.keys())
+        print('\r{}:{} -- ({})'.format(d['filename'],round(d['downloaded_bytes']/d['total_bytes']*100,2), round(d['elapsed'],3)),end='')
+
+
+ydl_opts = {
+    'format': 'bestaudio/best',
+    # 'verbose': 'True',
+    # 'verbose': 'False',
+    'postprocessors': [{
+        'key': 'FFmpegExtractAudio',
+        'preferredcodec': 'mp3',
+        'preferredquality': '2',
+        # 'preferredquality': '128k',
+    }],
+    # 'logger': MyLogger(),
+    # 'progress_hooks': [my_hook],
+}
+
+
+# regex for parsing track titles -- see ref/regex_text.txt
+track_exp = re.compile(r'^(?P<num>\d*)\.?\s*\W*(?P<title>.*[a-zA-z0-9]\)?)\s*(\(\))?')
+
+# regex to parse title and artist from title -- see ref/regex_test.txt
+album_exp = re.compile(r'(?:(?:\s*[\(|\[]\s*)?full\s*(?:album|ep)(?:[\)|\]]\s*)?'
+                                       r'(?:\s*stream(?:ing)?)?)\s*|(?:\s*[\!-\-\/\|\:]\s*)', re.IGNORECASE)
+# parsed = [tuple(filter(None, album_exp.split(album))) for album in albums]
+
+bad_exps = {'full album', 'full ep', 'streaming'  '-', ' ', '\t', '\n', '\r', '\x0b', '\x0c'}
+
 def make_safe(name):
-    illegal_chars = frozenset('\/|:*?<>"\'')
-    return ''.join((char for char in name if char not in illegal_chars))
+    illegal_chars = frozenset(r'\'\/:*?"<>|')
+    illegal_names = {'con', 'prn', 'aux', 'nul', 'com1', 'com2', 'com3', 'com4', 'com5', 'com6', 'com7', 
+                                'com8', 'com9', 'lpt1', 'lpt2', 'lpt3', 'lpt4', 'lpt5', 'lpt6', 'lpt7', 'lpt8', 'lpt9'}
+    f_name = ''.join((char for char in name if char not in illegal_chars)).strip()
+    for bad_name in illegal_names:
+        if f_name.lower()  == bad_name:
+            f_name = '{}_safe'.format(f_name)
+    return f_name
 
+def filter_list(full_list, excludes=(None, '')):
+    return (x for x in full_list if x not in set(excludes))
 
-def get_from_yt(url, bitrate, extension, output, verbose):
-    logger.info('fetching: {}'.format(url))
-    best_audio = pafy.new(url).getbestaudio()
-    logger.debug('fetched audiostream: {}'.format(best_audio.title, best_audio))
-    if int(best_audio.bitrate[:-1]) < bitrate:
-        raise BitrateException()
-    if output:
-        tempfile = os.path.join(output, make_safe('.'.join([best_audio.title, best_audio.extension])))
-        new_file = os.path.join(output, make_safe('.'.join([best_audio.title, extension])))
-    else:
-        new_file = make_safe('.'.join([best_audio.title, extension]))
-        tempfile = make_safe('.'.join([best_audio.title, best_audio.extension]))
-    best_audio.download(filepath=tempfile, quiet=not(verbose > 1))
-    logger.debug('temp file created: {}'.format(tempfile))
-    logger.info('converting downloaded stream...')
-    
-    # todo: check on adding metadata from here
-    AudioSegment.from_file(tempfile).export(new_file, format=args.extension, bitrate=args.bitrate).close()
-    logger.debug('removing temp file: {}'.format(tempfile))
-    os.remove(tempfile)
-    return new_file
+def parse_track(track_dict, num):    
+    # track_dict.update(track_exp.match(track_dict['title']).groupdict())
+    # if not track_dict['num']:
+    track_dict['track'] = num
 
-def make_args_dict(url, bitrate, extension, output, verbose):
-    """
-    shamelessly ripped from their source and converted to dict 
-    """
-    dict = {}
-    dict['artist_url'] = url
-    dict['num-tracks'] = sys.maxsize
-    dict['group'] = False
-    dict['bandcamp'] = False
-    dict['mixcloud'] = False
-    dict['audiomack'] = False
-    dict['hive'] = False
-    dict['likes'] = False
-    dict['login'] = 'soundscrape123@mailinator.com'
-    dict['downloadable'] = False
-    dict['track'] = ''
-    dict['folders'] = False
-    dict['path'] = output
-    dict['password'] = 'soundscraperocks'
-    dict['open'] = False
-    dict['keep'] = False
-    dict['version'] = False 
-    return dict
-    
-    
-def get_from_soundcloud(url, bitrate, extension, output, verbose):
-    raise NotImplementedError('Not yet implemented')
-    
-    logger.info('fetching: {}'.format(url))
-    sc.process_soundcloud(make_args_dict(url, bitrate, extension, output, verbose))
-    
-    # todo: change base audio
-    if int(best_audio.bitrate[:-1]) < bitrate:
-        raise BitrateException()
-    
-    if output:
-        tempfile = os.path.join(output, make_safe('.'.join([best_audio.title, best_audio.extension])))
-        new_file = os.path.join(output, make_safe('.'.join([best_audio.title, extension])))
-    
-    else:
-        new_file = make_safe('.'.join([best_audio.title, extension]))
-        tempfile = make_safe('.'.join([best_audio.title, best_audio.extension]))
-        
-    best_audio.download(filepath=tempfile, quiet=not(verbose > 1))
-    
-    logger.debug('temp file created: {}'.format(tempfile))
-    logger.info('converting downloaded stream...')
-    # todo: check on adding metadata from here
-    AudioSegment.from_file(tempfile).export(new_file, format=args.extension, bitrate=args.bitrate).close()
-    logger.debug('removing temp file: {}'.format(tempfile))
-    os.remove(tempfile)
-    return new_file
-    
-def get_from_bandcamp(url, bitrate, extension, output, verbose):
-    raise NotImplementedError('Not yet implemented')
-    
-if __name__ == '__main__':
-    args,extras = parser.parse_known_args()
+def parse_album(info_dict):
+    base_set = re.split('\w|/-\s+', info_dict['title'])
+    parsed_res = tuple(filter(None, album_exp.split(info_dict['title'])))
+    return parsed_res
+
+def gen_metadata(tag_dict):
+    accepted_keys = { "album","composer","genre","copyright","encoded_by","title","language",
+                                  "artist","album_artist","performer","disc","publisher","track","encoder","lyrics"}
+    safe_dict = {key:val for key,val in tag_dict.items() if key in accepted_keys}
     try:
-        extras = vars(extras)
-    except TypeError:
+        safe_dict['track'] = str(tag_dict['num'])
+    except KeyError:
         pass
-    converted = 0
-    failed = 0
+    return safe_dict
+    
+def slice_chapters(origin_media,track_list,quality='2',ext=None, add_metadata=True):
+    if ext is None:
+        base,origin_ext = osp.splitext(origin_media)
+        origin_ext = origin_ext[1:]
+        ext = origin_ext
+    else:
+        base,origin_ext = osp.splitext(origin_media)
+        origin_ext = origin_ext[1:]
+    base = osp.dirname(base)
+    logger.info('loading origin media %s...', origin_media)
+    origin = AudioSegment.from_file(origin_media,origin_ext)
+    logger.info('loaded origin media %s',origin_media)
+    for chapter in track_list:
+        # print(chapter)
+        path = osp.join(base,OUTPUTDIR,chapter['album'])
+        try:
+            os.makedirs(path)
+        except FileExistsError:
+            pass
+        track = '{0:02d}-{1}'.format(chapter['track'],chapter['title'])
+        try:
+            logger.info('slicing:%s',track)
+        except UnicodeEncodeError:
+            logger.info('slicing:%s', chapter['track'])
+        track_name = make_safe(track)
+        file_name = osp.join(path,'.'.join([track_name,ext]))
+        time_frame = [chapter[val]*1000 for val in ['start_time','end_time']]
+        segment = origin[time_frame[0]:time_frame[1]]
+        try:
+            logger.info('saving as %s', file_name)
+        except UnicodeEncodeError:
+            logger.info('saving file to %s', path)
+        if add_metadata:
+            logger.info('populating metadata')
+            segment.export(file_name,format=ext,parameters=["-q:a", quality], tags=gen_metadata(chapter))
+        else:
+            segment.export(file_name,format=ext,parameters=["-q:a", quality])
+        logger.info('Done!\n')
+    
+
+def get_info(url,ydl_opts):
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        info = ydl.extract_info(url, download=False)
+        # print(info.keys(),end='\n\n')
+        logger.info('title: %s', info.get('title','N/A'))
+        logger.info('creator: %s', info.get('uploader',info.get('id','N/A'))) 
+        logger.debug('bit_rate: %s', info.get('abr','N/A'))
+        logger.debug('format: %s', info.get('ext','N/A'))
+        
+        num = 0
+        track_list = []
+        try:
+            album_info = parse_album(info)
+            if len(album_info) == 1:
+                album = album_info[0]
+            else:
+                album = album_info[1]
+            key = 'chapters' if info.get('chapters', None) is not None else 'entries'
+            logger.info('%s:',key)
+            for chapter_dict in info.get(key, []):
+                num += 1
+                chapter_dict['album'] = album
+                try:
+                    chapter_dict['artist'] = info['uploader']
+                except KeyError:
+                    logger.warning('no artist found')
+                parse_track(chapter_dict,num)
+                # for item in chapter_dict:
+                    # print(item, chapter_dict[item] if item not in ['url','http_headers','formats'] else 'LEN', sep=': ')
+                # print('')
+                try:
+                    logger.info('\ttrack:{track}\n\ttitle:{title}\n\tstart:{start_time}\n\tstop:{end_time}\n'.format(**chapter_dict))
+                except:
+                    logger.warning('failed to print info')
+                track_list.append(chapter_dict)
+            # else:
+                # logger.debug('Finished looping through chapters')
+        except (TypeError, KeyError):
+            logger.warning('no timestamp info %s',url)
+        return  info, track_list
+    
+def grab_file(url,ydl_opts,convert=True, info=None, track_list=None):
+    if info is None and track_list is None:
+        info, track_list = get_info(url,ydl_opts)
+    if not convert:
+        # info['ext'] = ydl_opts['postprocessors'][0]['preferredcodec'] 
+        opts = {k:v for k,v in ydl_opts.items() if k not in {'postprocessors'}}
+    else: 
+        opts = ydl_opts
+    
+    logger.info('starting download...')
+    with youtube_dl.YoutubeDL(opts) as ydl:
+        # import pdb; pdb.set_trace()
+        ydl.download([url])
+        outfile = ydl.prepare_filename(info)
+    logger.info('Done!')
+    return outfile, track_list
+        
+if __name__ == '__main__':
+    args = parser.parse_args()
 
     if args.verbose :
         if args.verbose > 1:
             logger.setLevel(logging.DEBUG)
             ch.setLevel(logging.DEBUG)
+            ydl_opts['verbose']=  'True'
         else:
             logger.setLevel(logging.INFO)
             ch.setLevel(logging.INFO)
     elif args.quiet:
         logger.setLevel(logging.CRITICAL)
         ch.setLevel(logging.CRITICAL)
-        if args.quiet > 1:
+        ydl_opts['verbose']=  'False'
+        if args.quiet > 3:
             logger.disabled = True
-
+    quality = args.quality if not args.bitrate else args.bitrate
+    ydl_opts['postprocessors'][0]['preferredcodec'] = args.extension
+    ydl_opts['postprocessors'][0]['preferredquality'] =  quality
+    
     if args.file:
         logger.info('reading urls from file...\n')
         urls = open(args.urls[0], 'r')
@@ -191,32 +302,16 @@ if __name__ == '__main__':
 
     for url in urls:
         if not url.startswith(COMMENT):
-            try:
-                if not (args.soundcloud or args.bandcamp):
-                    new_file = get_from_yt(url, int(args.bitrate[:-1]), args.extension, args.output, args.verbose)
-                elif args.soundcloud:
-                    new_file = get_from_soundcloud(url, int(args.bitrate[:-1]), args.extension, args.output, args.verbose)
-                elif args.bandcamp:
-                    new_file = get_from_soundcloud(url, int(args.bitrate[:-1]), args.extension, args.output, args.verbose)
-                    
-                converted += 1
-                try:
-                    logger.info('converted: {}'.format(os.path.basename(new_file)))
-                except UnicodeEncodeError:
-                    logger.info('converted: {}'.format(url))
-                logger.debug('converted file located at: {}'.format(os.path.dirname(os.path.abspath(new_file))))
-            except Exception as e:
-                failed += 1
-                logger.warning('failed to convert: {}'.format(url))
-                logger.warning(e)
-
-    if not (converted | failed):
-        logger.warning('no identifiers found to convert')
-    else:
-        logger.warning('converted: {} entries'.format(converted))
-        logger.warning('failed to convert: {} entries'.format(failed))
+            info, track_list = get_info(url,ydl_opts)
+            convert = True if not track_list else False  # delay conversion until slicing happens
+            outfile, track_list = grab_file(url,ydl_opts,convert=convert, info=info, track_list=track_list)
+            if track_list and PYDUB:
+                # begin pydub chapter slicing:
+                logger.info('slicing chapters from origin...')
+                slice_chapters(outfile,track_list,quality=quality,ext=args.extension,add_metadata=(not args.no_metadata))
     try:
         urls.close()
     except AttributeError:
         pass
-    sys.exit(0)
+        
+        
